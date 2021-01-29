d081f6b49efde3051f21b42e49d3bf10f9c7668f
Dx724 <dx72dx@gmail.com>
2021-01-28 19:18:10 -0500

    Add realtime BTC API

--- StockLight/stock_light.py
+++ StockLight/stock_light.py
@@ -1,22 +1,30 @@
 import requests
-import json
-from collections import OrderedDict
 
 with open("secret.txt", "r") as f:
     secret = f.read().replace("\n", "")
 
-print(secret)
-
 TICKER = "GME"
-
+'''
 req = requests.get("https://www.alphavantage.co/query", {
+        "function": "GLOBAL_QUOTE", "symbol": TICKER, "apikey": secret
+    })
+'''
+
+'''
         "function": "TIME_SERIES_INTRADAY", "symbol": TICKER, "apikey": secret,
         "interval": "1min", "outputsize": "compact", "datatype": "json"
+'''
+req = requests.get("https://www.alphavantage.co/query", {
+        "function": "CURRENCY_EXCHANGE_RATE", "apikey": secret,
+        "from_currency": "BTC", "to_currency": "USD"
     })
 
 if (req.status_code != 200):
     print("Request error: {}".format(req.status_code))
 else:
+    response = req.json()
+    #price = response["Global Quote"]["05. price"]
+    '''
     # Note that while dictionaries are ordered in Python 3.7+, this is
     # simply an implementation detail. Thus, use OrderedDict to ensure
     # compatibility with past and future versions.
@@ -24,5 +32,7 @@ else:
 
     #Obtain most recent price from time series
     price = next(iter(response["Time Series (1min)"].items()))[1]["4. close"]
+    '''
+    price = response["Realtime Currency Exchange Rate"]["5. Exchange Rate"]
     pr = float(price)
-    print(response)
+    print(pr)
